8ab0be4c5df159b5e14eff5f4e25ff9d63377ed9
Yuri <seuemail@gmail.com>
2026-04-01 12:25:10 -0300

    fix bcrypt error

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const pool = require("../config/db");
 const auth = require("../middleware/auth");
 const role = require("../middleware/role");
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const pool = require("./config/db");
 
 const app = express();
