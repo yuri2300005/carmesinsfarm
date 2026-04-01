a6a7495523a6e38c2a02115c74bc910c629a8fce
Yuri <seuemail@gmail.com>
2026-04-01 13:38:34 -0300

    frontend dentro do backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://carmesinsfarm-production.up.railway.app/api"
+  baseURL: "/api"
 });
 
 api.interceptors.request.use((config) => {
